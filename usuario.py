fd302720bae7ccc0ccde3b73a81c3b2e41ab5aba
mjdlsandov <mariajosedeleonsandoval@gmail.com>
2026-05-26 01:30:07 -0600

    Delete usuario.py

--- usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-
-    def __init__(self, nombre):
-        self.nombre = nombre
-        self.imagenes = []
-
-    def agregar_imagen(self, imagen):
-        self.imagenes.append(imagen)
