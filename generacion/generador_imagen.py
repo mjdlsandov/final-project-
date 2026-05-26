7ec7ddeb44f5c92da1583e8b7cf05470bc60a0e8
mjdlsandov <mariajosedeleonsandoval@gmail.com>
2026-05-26 01:42:32 -0600

    Add files via upload

--- generacion/generador_imagen.py
+++ generacion/generador_imagen.py
@@ -1,1 +1,18 @@
+from PIL import Image
 
+
+def generar_imagen():
+
+    img = Image.new("RGB", (200, 200), "white")
+
+    pixels = img.load()
+
+    pixels[50, 50] = (255, 0, 0)
+
+    pixels[51, 50] = (255, 0, 0)
+
+    pixels[52, 50] = (255, 0, 0)
+
+    img.save("imagen_generada.png")
+
+    print("Imagen generada")
